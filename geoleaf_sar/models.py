2bd23fedad4aee76414321744d20a8db5255065d
unknown <lizthompson07@gmail.com>
2020-09-18 16:34:41 -0300

    set up postgre and geodjango

--- geoleaf_sar/models.py
+++ geoleaf_sar/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+
+class Species(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.PointField()
+    status = models.CharField(max_length=100)
+    note = models.CharField(max_length=100)
